96b3aa7e554df04425acd33c2c50bcd8410c0800
Caio Araújo <caiocaladaraujo.dev@gmail.com>
2025-03-28 23:47:13 -0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_training/main.dart';
+import 'package:flutter_training/core/theme/material_settings.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialSettings(
+      color: 0,
+      isDarkMode: false,
+      isLogged: false,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
