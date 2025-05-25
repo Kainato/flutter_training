2fc56f03c004f5d80f1dc70c090a0269b82163ff
Kainato <caiocaladaraujo@gmail.com>
2025-05-24 22:01:23 -0300

    Update build_context_extension.dart
    theme extesion

--- lib/extension/build_context_extension.dart
+++ lib/extension/build_context_extension.dart
@@ -108,6 +108,23 @@ extension BuildContextExtension on BuildContext {
   Color get surfaceContainerHighestColor =>
       Theme.of(this).colorScheme.surfaceContainerHighest;
 
+  // * TEXT THEME EXTENSIONS
+  TextStyle get titleLarge => Theme.of(this).textTheme.titleLarge!;
+  TextStyle get titleMedium => Theme.of(this).textTheme.titleMedium!;
+  TextStyle get titleSmall => Theme.of(this).textTheme.titleSmall!;
+  TextStyle get bodyLarge => Theme.of(this).textTheme.bodyLarge!;
+  TextStyle get bodyMedium => Theme.of(this).textTheme.bodyMedium!;
+  TextStyle get bodySmall => Theme.of(this).textTheme.bodySmall!;
+  TextStyle get labelLarge => Theme.of(this).textTheme.labelLarge!;
+  TextStyle get labelMedium => Theme.of(this).textTheme.labelMedium!;
+  TextStyle get labelSmall => Theme.of(this).textTheme.labelSmall!;
+  TextStyle get displayLarge => Theme.of(this).textTheme.displayLarge!;
+  TextStyle get displayMedium => Theme.of(this).textTheme.displayMedium!;
+  TextStyle get displaySmall => Theme.of(this).textTheme.displaySmall!;
+  TextStyle get headlineLarge => Theme.of(this).textTheme.headlineLarge!;
+  TextStyle get headlineMedium => Theme.of(this).textTheme.headlineMedium!;
+  TextStyle get headlineSmall => Theme.of(this).textTheme.headlineSmall!;
+
   // * SNACKBAR EXTENSIONS
   void showSnackBar(
     String message, {
